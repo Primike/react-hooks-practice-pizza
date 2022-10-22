4e02b1a07bfb29f9cd64c283998d479e277bfd30
Prince <avecillasprince@yahoo.com>
2022-10-22 18:39:28 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+  const [selectedPizza, setSelectedPizza] = useState({id: '', topping: '', size: 'Small', vegetarian: false})
+  
+  useEffect(() => {
+    fetchPizzas()
+  }, [])
+
+  function fetchPizzas() {
+    fetch(`http://localhost:3001/pizzas`)
+      .then(response => response.json())
+      .then(json => setPizzas(json))
+  }
+
+  function editPizza(key, value) {
+    setSelectedPizza({...selectedPizza, [key]: value})
+  }
+
+  function changePizza(e) {
+    e.preventDefault();
+    if(selectedPizza.id === '') {
+      return
+    }
+    
+    fetch(`http://localhost:3001/pizzas/${selectedPizza.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(selectedPizza),
+    })
+      .then((response) => response.json())
+      .then(fetchPizzas);
+  }
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm selectedPizza = {selectedPizza} editPizza = {editPizza} changePizza = {changePizza}/>
+      <PizzaList pizzas = {pizzas} setSelectedPizza = {setSelectedPizza}/>
     </>
   );
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function PizzaForm() {
+function PizzaForm({selectedPizza, editPizza, changePizza}) {
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={changePizza}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -10,10 +10,13 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={selectedPizza.topping}
+            onChange = {(e) => {editPizza(e.target.name, e.target.value)}}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select className="form-control" name="size" value = {selectedPizza.size}
+                  onChange = {(e) => {editPizza(e.target.name, e.target.value)}}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -26,6 +29,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={selectedPizza.vegetarian}
+              onChange={(e) => {editPizza(e.target.name, true)}}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -35,6 +40,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={!selectedPizza.vegetarian}
+              onChange={(e) => {editPizza(e.target.name, false)}}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
